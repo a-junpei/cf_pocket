892819b4412c89cdd476900f392da1a2f1ac26c0
Arikawa Junpei <junpei.1982@gmail.com>
2021-12-27 18:49:28 +0900

    keyを環境変数から取るように変更

--- functions/api/list.ts
+++ functions/api/list.ts
@@ -1,8 +1,14 @@
-export async function onRequestGet(request) {
+export async function onRequestGet(context) {
+    const {
+      request,
+      env,
+      params,
+    } = context;
+
     const url = "https://getpocket.com/v3/get";
     const obj = {
-        consumer_key: "****",
-        access_token: "****",
+        consumer_key: env.CONSUMER_KEY,
+        access_token: env.ACCESS_CODE,
         state: "unread",
         count: 10,
         offset: 0,
@@ -14,6 +20,7 @@ export async function onRequestGet(request) {
     const headers = {
       'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
     };
-    const response = fetch(url, {method, headers, body});
+    // const response = fetch(url, {method, headers, body});
+    const response = new Response(JSON.stringify(obj));
     return response;
 }
